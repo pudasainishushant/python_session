0b8a5bc0b849dd1c853f7881200ed36131afa21c
pudasainishushant <shushanta574@gmail.com>
2021-06-21 19:12:55 +0545

    day 3

--- simple_calculator.py
+++ simple_calculator.py
@@ -25,14 +25,29 @@ def add_num(num1,num2):
     return: result : int
     """
     result = num1 + num2
+    sub_result = num1 - num2
+    # print("Addition ko result chai {} ho hai boro".format(result))
     return result
 
+
+def sub_num(num1,num2):
+    """
+    Add two given numbers
+    params: num1: int
+            num2: int
+    return: result : int
+    """
+    result = num1 - num2
+    # sub_result = num1 - num2
+    # print("Addition ko result chai {} ho hai boro".format(result))
+    return result
 
 
 if __name__ == "__main__":
     num1 = int(input("Enter first number \t"))
     # print("Data type of first variable is ",type(num1))
     num2 = int(input("Enter second number \t"))
-    result_addition = add_num(num1,num2)
-    print("Addition result is -> ",result_addition)
-    print("The addition of {} and {} is {}".format(num1,num2,result_addition))
+    result,sub_result,div_result = add_num(num1,num2)
+    # print(type(result))
+    # print("Addition result is -> ",result_addition)
+    print("The addition of {} and {} is {}".format(num1,num2,result))
--- test.py
+++ test.py
@@ -1,6 +1,37 @@
 # import flask
-from simple_calculator import add_num
+from simple_calculator import add_num,sub_num
 
-result = add_num(10,11)
-print(result)
+num1 = 5
+num2 = 4
+program = input("What program is this? /n")
 
+
+if program == "Calculator":
+    decision = input("Addition or Subtraction")
+    if decision == "Addition":   
+        addition_result = add_num(num1,num2)
+        print(addition_result")
+    else:
+        result = sub_num(num1,num2)
+        print(result)
+elif program == "Printer":
+    print("hahha")
+else:
+    print("Yo print garxa ani tyo loop bata pass hunxa")
+    pass
+
+
+
+"""
+if onekeystroke == "forward" and secondkeystroke == "a":
+    staetment 1 gadi agadi badaune statemnt
+    staemtn 2
+elif condition2:
+    stamtnemt 3
+    statemtn 4
+elif condition3:
+    statemtn 5
+else:
+    statemtn 6
+
+"""
